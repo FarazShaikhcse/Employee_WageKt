27a8279dd5d3154a800515a02107c6d1f8bcf1db
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-10-10 23:39:28 +0530

    storing daily wage

--- Main.kt
+++ Main.kt
@@ -15,17 +15,16 @@ class EmpWageBuilder : WageBuilder{
                 companyEmpWage.add(CompanyEmpWage(company, wagePerHour, maxHoursPerMonth, maxDaysPerMonth))
                 companyEmpWage[numOfCompany]!!.computeEmpWage()
                 numOfCompany += 1
-
         }
 }
 fun main() {
+
         println("Welcome to employee wage computation")
         val empWageBuilder = EmpWageBuilder()
         val n: Int
         val scanner = Scanner(System.`in`)
         println("Enter the no of companies")
         n = scanner.nextInt()
-        val employeeWage: Array<CompanyEmpWage?> = arrayOfNulls<CompanyEmpWage>(n)
         for (i in 0 until n) {
                 println("Enter the company name:")
                 val name: String = scanner.next()
